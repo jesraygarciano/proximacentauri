134ff0e9a3b8439e85d8588d284157ee5cf15d98
Jesray Garciano <jesray.garciano@gmail.com>
2018-04-20 13:41:22 +0800

    Model changes

--- app/CharacterReferences.php
+++ app/CharacterReferences.php
@@ -10,8 +10,7 @@ class CharacterReferences extends Model
     protected $table = 'character_references';
 
     protected $fillable = ['cr_name', 'cr_position', 'cr_company_or_university', 'cr_phone_number', 'cr_email'];
-    //
-    // protected $hidden = ['password'];
+    
     public function resume()
     {
         return $this->belongsTo(Resume::class);
--- app/Company.php
+++ app/Company.php
@@ -6,12 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    // dd('通りました');
     protected $fillable = ['company_name', 'email', 'password', 'in_charge', 'ceo_name', 'postal', 'address1', 'address2', 'city', 'country', 'url', 'tel', 'number_of_employee', 'established_at', 'facebook_url', 'twitter_url', 'company_logo', 'background_photo', 'company_introduction', 'what', 'what_photo1', 'what_photo1_explanation', 'what_photo2', 'what_photo2_explanation', 'bill_company_name', 'bill_postal', 'bill_address1', 'bill_address2', 'bill_city', 'bill_country', 'user_id', 'created_at', 'updated_at', 'is_active', 'company_size'];
 
-    // dd('通りました');
-
-    // protected $hidden = ['password'];
     protected $appends = ['population'];
 
     public function openings()
@@ -71,11 +67,6 @@ class Company extends Model
                 break;
         }
     }
-
-    /*public function company_google_mapper()
-    {
-        return $this->attributes['address1']. " ". $this->attributes['city']. " ". $this->attributes['country'];
-    }*/
 
     public function getCompanyLogoAttribute(){
         if(!file_exists('storage/'.$this->attributes['company_logo']) || str_replace(' ','',$this->attributes['company_logo']) == ''){
