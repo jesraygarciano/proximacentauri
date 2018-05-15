c41897b1d5dccdde7347b66cb8f9cbeb6fc876e1
Jesray Garciano <jesray.garciano@gmail.com>
2018-05-15 17:07:11 +0800

    Helper updates

--- app/Jobs/ProcessEmail.php
+++ app/Jobs/ProcessEmail.php
@@ -12,17 +12,14 @@ class ProcessEmail implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $email;
-
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($email)
+    public function __construct()
     {
         //
-        $this->email = $email;
     }
 
     /**
@@ -32,13 +29,6 @@ class ProcessEmail implements ShouldQueue
      */
     public function handle()
     {
-        $recipient = $this->email['recipient'];
-        $sender = $this->email['sender'];
-        $subject = $this->email['subject'];
-
-        \Mail::send(isset($this->email['blade'])?$this->email['blade']:'', $this->email, function ($m) use ($recipient,$sender,$subject) {
-            $m->from($sender->email, $sender->name);
-            $m->to($recipient->email, $recipient->name)->subject($subject);
-        });
+        //
     }
 }
